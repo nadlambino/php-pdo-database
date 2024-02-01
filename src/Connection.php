acadfd06dc82b7e52fca4110dde0fbd84d8ecd2e
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-02-01 18:35:20 +0800

    docs: docblocks

--- src/Connection.php
+++ src/Connection.php
@@ -14,25 +14,39 @@ use Inspira\Database\Connectors\MySql;
 use Inspira\Database\Connectors\PgSql;
 use Inspira\Database\Connectors\Sqlite;
 use PDO;
+use RuntimeException;
 use Symfony\Component\String\Inflector\EnglishInflector;
 use Symfony\Component\String\Inflector\InflectorInterface;
 
+/**
+ * Class Connection
+ *
+ * The Connection class is responsible for managing and creating PDO connections to different database drivers.
+ * It supports MySQL, PostgreSQL, and SQLite database connections.
+ *
+ * @package Inspira\Database
+ */
 class Connection
 {
 	/**
+	 * An array to store and retrieve PDO instances based on connection names.
+	 *
 	 * @var array<string, PDO> $pool
 	 */
 	private static array $pool = [];
 
+	/**
+	 * The name of the database connection. Defaults to 'default'.
+	 */
 	private string $name = 'default';
 
+	/**
+	 * @param Container $container  A dependency injection container for managing class dependencies.
+	 * @param Config $config A config class instance.
+	 */
 	public function __construct(protected Container $container, protected Config $config)
 	{
-		$this->registerDatabaseServices();
-	}
-
-	protected function registerDatabaseServices(): void
-	{
+		// Registering singletons and bindings in the container.
 		$this->container->singleton(PDO::class, fn() => $this->create());
 		$this->container->singleton(InflectorInterface::class, EnglishInflector::class);
 		$this->container->bind(Query::class);
@@ -43,6 +57,12 @@ class Connection
 		$this->container->bind('sqlite', Sqlite::class);
 	}
 
+	/**
+	 * Set the name of the database connection.
+	 *
+	 * @param string $name The name of the database connection.
+	 * @return $this
+	 */
 	public function name(string $name): self
 	{
 		$this->name = $name;
@@ -51,35 +71,42 @@ class Connection
 	}
 
 	/**
-	 * @return PDO
-	 * @throws Exception
+	 * Create a new PDO connection based on the configured database connection.
+	 *
+	 * @return PDO The PDO instance representing the database connection.
+	 * @throws Exception If there are issues creating the PDO connection.
 	 */
 	public function create(): PDO
 	{
+		// If the connection already exists in the pool, return it.
 		if (Connection::has($this->name)) {
 			return Connection::get($this->name);
 		}
 
+		// Get the connection name from the configuration.
 		$name = $this->config->get("database.$this->name");
 
+		// Throw an exception if the connection name is not found in the configuration.
 		if (empty($name)) {
-			throw new Exception("Unknown connection name `$this->name`.");
+			throw new RuntimeException("Unknown connection name `$this->name`.");
 		}
 
+		// Get the connection configurations from the configuration file.
 		$configs = $this->config->get("database.connections.$name");
 
+		// Throw an exception if the connection configurations are not found.
 		if (empty($configs)) {
-			throw new Exception("Unknown connection configuration `$name`.");
+			throw new RuntimeException("Unknown connection configuration `$name`.");
 		}
 
 		if (!is_array($configs)) {
-			throw new Exception("Connection structure must be an array containing its configurations.");
+			throw new RuntimeException("Connection structure must be an array containing its configurations.");
 		}
 
 		$connector = $this->container->getConcreteBinding($driver = $configs['driver']);
 
 		if (empty($connector)) {
-			throw new Exception("Connector class for `$driver` driver is not found.");
+			throw new RuntimeException("Connector class for `$driver` driver is not found.");
 		}
 
 		$timezone = $this->config->get('database.timezone') ?? $this->config->get('app.timezone', '+00:00');
@@ -92,22 +119,39 @@ class Connection
 		};
 
 		if (!($connection instanceof ConnectorInterface)) {
-			throw new Exception("Connector class for `$driver` driver must be an instance of " . ConnectorInterface::class);
+			throw new RuntimeException("Connector class for `$driver` driver must be an instance of " . ConnectorInterface::class);
 		}
 
 		return static::$pool[$this->name] = $connection->connect();
 	}
 
+	/**
+	 * Check if a connection with the specified name exists in the pool.
+	 *
+	 * @param string $name The name of the database connection.
+	 * @return bool True if the connection exists, false otherwise.
+	 */
 	public static function has(string $name): bool
 	{
 		return isset(self::$pool[$name]);
 	}
 
+	/**
+	 * Get the PDO instance for the specified connection name.
+	 *
+	 * @param string $name The name of the database connection.
+	 * @return PDO|null The PDO instance if found, null otherwise.
+	 */
 	public static function get(string $name): ?PDO
 	{
 		return self::$pool[$name] ?? null;
 	}
 
+	/**
+	 * Get all connections stored in the pool.
+	 *
+	 * @return array An array of connection names and corresponding PDO instances.
+	 */
 	public static function all(): array
 	{
 		return self::$pool;
