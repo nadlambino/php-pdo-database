5f2de4c09efcc33cd7ce33b0e9236d3c320c19ec
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-02-11 19:27:23 +0800

    ref: separate the connection pool and connection factory into their own class

---
+++ src/ConnectionFactory.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inspira\Database;
+
+use Closure;
+use Inspira\Container\Container;
+use Inspira\Database\Connectors\ConnectorInterface;
+use Inspira\Database\Connectors\MySql;
+use Inspira\Database\Connectors\PgSql;
+use Inspira\Database\Connectors\Sqlite;
+use PDO;
+use RuntimeException;
+
+/**
+ * Class ConnectionFactory
+ *
+ * The ConnectionFactory class is responsible for creating a PDO connection.
+ * It is also responsible for binding the supported connectors into the container and bind a singleton instance of the created PDO instance.
+ */
+class ConnectionFactory
+{
+	public function __construct(protected Container $container, protected array $config, protected string $name = 'default')
+	{
+		$this->container->bind('mysql', MySql::class);
+		$this->container->bind('pgsql', PgSql::class);
+		$this->container->bind('sqlite', Sqlite::class);
+	}
+
+	public function create(): PDO
+	{
+		$connectionName = $this->config[$this->name] ?? null;
+
+		if (empty($connectionName)) {
+			throw new RuntimeException("Unknown connection name `$this->name`.");
+		}
+
+		if (!isset($this->config['connections'])) {
+			throw new RuntimeException("Connections configurations are not defined in the configuration array.");
+		}
+
+		$configuration = $this->config['connections'][$connectionName];
+
+		if (!isset($configuration)) {
+			throw new RuntimeException("Unknown connection configuration `$connectionName`.");
+		}
+
+		$connector = $this->container->getConcreteBinding($driver = $configuration['driver']);
+
+		if (empty($connector)) {
+			throw new RuntimeException("Connector class for `$driver` driver is not found.");
+		}
+
+		$timezone = $this->config['timezone'] ?? '+00:00';
+		$configuration['timezone'] = strtolower($timezone) === 'utc' ? '+00:00' : $timezone;
+
+		$connection = match (true) {
+			is_string($connector) => new $connector($configuration),
+			$connector instanceof Closure => $this->container->resolve($connector),
+			default => $connector
+		};
+
+		if (!($connection instanceof ConnectorInterface)) {
+			throw new RuntimeException("Connector class for `$driver` driver must be an instance of " . ConnectorInterface::class);
+		}
+
+		$pdo = $connection->connect();
+
+		$this->container->singleton(PDO::class, fn(): PDO => $pdo);
+
+		return $pdo;
+	}
+}
---
+++ src/ConnectionPool.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inspira\Database;
+
+use Closure;
+use Exception;
+use Inspira\Container\Container;
+use Inspira\Database\Builder\Query;
+use Inspira\Database\Connectors\ConnectorInterface;
+use Inspira\Database\Connectors\MySql;
+use Inspira\Database\Connectors\PgSql;
+use Inspira\Database\Connectors\Sqlite;
+use PDO;
+use RuntimeException;
+use Symfony\Component\String\Inflector\EnglishInflector;
+use Symfony\Component\String\Inflector\InflectorInterface;
+
+/**
+ * Class ConnectionPool
+ *
+ * The ConnectionPool class is responsible for managing PDO connections to different database drivers.
+ *
+ * @package Inspira\Database
+ */
+class ConnectionPool
+{
+	/**
+	 * An array to store and retrieve PDO instances based on connection names.
+	 *
+	 * @var array<string, PDO> $pool
+	 */
+	private array $pool = [];
+
+	/**
+	 * @param Container $container A dependency injection container for managing class dependencies.
+	 * @param array $config An array of database configurations.
+	 */
+	public function __construct(protected Container $container, protected array $config)
+	{
+	}
+
+	/**
+	 * Check if a connection with the specified name exists in the pool.
+	 *
+	 * @param string $name The name of the database connection.
+	 * @return bool True if the connection exists, false otherwise.
+	 */
+	public function has(string $name): bool
+	{
+		return isset($this->pool[$name]);
+	}
+
+	/**
+	 * Get the PDO instance for the specified connection name.
+	 *
+	 * @param string $name The name of the database connection.
+	 * @return PDO|null The PDO instance if found, null otherwise.
+	 */
+	public function get(string $name): ?PDO
+	{
+		if ($this->has($name)) {
+			return $this->pool[$name];
+		}
+
+		$factory = new ConnectionFactory($this->container, $this->config, $name);
+		$this->pool[$name] = $factory->create();
+
+		return $this->pool[$name];
+	}
+
+	/**
+	 * Get all connections stored in the pool.
+	 *
+	 * @return array An array of connection names and corresponding PDO instances.
+	 */
+	public function all(): array
+	{
+		return $this->pool;
+	}
+}
